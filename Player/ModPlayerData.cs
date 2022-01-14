e8dcf37b5ac3015f8cf28c666284476a546b0408
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-01-14 10:03:39 -0300

    changes

--- Player/ModPlayerData.cs
+++ Player/ModPlayerData.cs
@@ -1,5 +1,3 @@
-using Terraria;
-using Terraria.DataStructures;
 using Terraria.ModLoader;
 
 namespace TerrariaTestMod.Player
@@ -8,13 +6,6 @@ namespace TerrariaTestMod.Player
   {
     public ModPlayerData()
     {
-    }
-
-    public override void Kill(double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource)
-    {
-      Main.PlaySound(SoundLoader.customSoundType, -1, -1, mod.GetSoundSlot(SoundType.Custom, Sounds.Oof));
-
-      base.Kill(damage, hitDirection, pvp, damageSource);
     }
   }
 }
--- TerrariaTestMod.cs
+++ TerrariaTestMod.cs
@@ -2,11 +2,6 @@ using Terraria.ModLoader;
 
 namespace TerrariaTestMod
 {
-  public static class Sounds
-  {
-    public static string Oof = "Sounds/Custom/OofSound";
-  }
-
   public class TerrariaTestMod : Mod
   {
   }
